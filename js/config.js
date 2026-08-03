166ceeba7553910d23fb27bf85c207406a485524
uru-08 <uru.1891@gmail.com>
2026-08-03 02:35:29 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://farpkwpxnnfloesivkxv.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_xDGhJja9MTcw8pOcypDITg_bRIbMg5E";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
