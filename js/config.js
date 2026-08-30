3249876cdb483539cdcbaadca8f4100cd669e0f6
uru-08 <uru.1891@gmail.com>
2026-08-30 10:27:20 -0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -13,3 +13,16 @@ function supabaseHeaders(extraHeaders = {}) {
     ...extraHeaders
   };
 }
+
+/* DENEXA V134: carga segura y exclusiva para el panel del comercio. */
+if (/\/comercio\.html$/i.test(window.location.pathname)) {
+  const denexaPanelStyle = document.createElement("link");
+  denexaPanelStyle.rel = "stylesheet";
+  denexaPanelStyle.href = "css/comercio-v134.css?v=134";
+  document.head.appendChild(denexaPanelStyle);
+
+  const denexaPanelFixes = document.createElement("script");
+  denexaPanelFixes.src = "js/comercio-v134.js?v=134";
+  denexaPanelFixes.defer = true;
+  document.head.appendChild(denexaPanelFixes);
+}
